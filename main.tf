5e9d17d2d82604b312aaf86793fe7263ebd0b136
Tenric00 <112815555+Tenric00@users.noreply.github.com>
2025-07-25 15:41:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,6 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  /* instance_type = "t3.nano" */
   instance_type = "t2.micro"
 
   tags = {
